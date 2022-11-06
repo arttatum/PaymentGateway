caf2142701cc346e8f890079b6db148a73e94935
William <woughney1@gmail.com>
2022-11-06 00:45:06 +0000

    [WL] Added tests for uuid guard clauses

---
+++ tests/shared_kernel/guard_clauses/test_uuid_guard.py
@@ -0,0 +1,22 @@
+import uuid
+from shared_kernel.guard_clauses.uuid_guard import is_valid_uuid
+import pytest
+@pytest.mark.parametrize("not_a_uuid4",
+[
+    123,
+    "abc",
+    {"heaven": "is",
+    "a": "halfpipe"}
+])
+def test_is_valid_uuid(not_a_uuid4):
+    assert not is_valid_uuid(not_a_uuid4)
+
+@pytest.mark.parametrize("a_uuid4",
+[
+    str(uuid.uuid4()),
+    str(uuid.uuid4()),
+    str(uuid.uuid4()),
+    str(uuid.uuid4()),
+    ])
+def test_is_valid_uuid(a_uuid4):
+    assert is_valid_uuid(a_uuid4)
